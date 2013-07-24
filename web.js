b55d7dcf32d3988bbef489311f8de935e843d61e
Ubuntu <ubuntu@ip-172-31-26-145.us-west-2.compute.internal>
2013-07-24 10:28:39 +0000

    everything works I think

--- web.js
+++ web.js
@@ -6,13 +6,16 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+
+var bensfilebuf = fs.readFileSync("index.html");
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send( bensfilebuf.toString() ) ;        //  'Hello World!');
 });
 
-var bensfile = fs.readFileSync("index.html");
+//var bensfile = fs.readFileSync("index.html"); 
+//var buffer = new Buffer(bensfile),"utf-8");
 
-console.log("Bens test fiel=" + bensfile);
+console.log("Bens test fiel=" + bensfilebuf);
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
