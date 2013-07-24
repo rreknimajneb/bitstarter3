0f5d664038a5604b41f722da822822cbaa76708c
Ubuntu <ubuntu@ip-172-31-26-145.us-west-2.compute.internal>
2013-07-24 10:07:00 +0000

    jesus christ it finally works

--- web.js
+++ web.js
@@ -1,3 +1,7 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+
 var express = require('express');
 var app = express();
 app.use(express.logger());
@@ -6,6 +10,9 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+var bensfile = fs.readFileSync("index.html");
+
+console.log("Bens test fiel=" + bensfile);
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
